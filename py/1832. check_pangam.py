255ac5bd49da35d198e11625ee959407d107c090
Fahd <asadmansa@icloud.com>
2024-10-31 16:10:28 -0400

    268

--- py/1832. check_pangam.py
+++ py/1832. check_pangam.py
@@ -1,6 +1,16 @@
+# 2
+# letters are lowercase
+class Solution:
+	def checkIfPangram(self, sentence: str) -> bool:
+			seen = set(sentence)
+			return len(seen) == 26
+
+
+#1
+# if letters are lowercase and uppercase
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-            o = set()
+            o = set(char)
             for i in range(len(sentence)):
                 char = sentence[i]
                 n = ord(char.lower()) - ord('a') + 1
