8886ddf9de4f9aa6abc8c066dae8ced8e2d84858
fahd <asadmansa@icloud.com>
2024-11-07 18:23:27 -0500

    1426

--- py/268. missing_number.py
+++ py/268. missing_number.py
@@ -1,7 +1,25 @@
+#3
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        s = set(nums) # 3, 0, 1
+        
+        for i in range(n + 1):
+            if i not in s:
+                return i
+        
+#2
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         seen = set(nums)
-        max = 0
+        for i in range(0, len(nums) + 1):
+            if i not in seen:
+                return i
+
+#1
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        seen = set(nums)
         for i in range(0, len(nums) + 1):
             if i not in seen:
                 return i
