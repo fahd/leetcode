ec0caa5f5a6aa8f618f72ec65671f9f90c33db7c
Fahd <asadmansa@icloud.com>
2024-12-01 18:08:02 -0500

    876

--- py/19 remove_nth_node.py
+++ py/19 remove_nth_node.py
@@ -8,6 +8,38 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         slow = head
         fast = head
+        for _ in range(n):
+            fast = fast.next
+        
+        '''
+
+        
+        [1,2,3,4,5]
+             s
+                 f
+
+        '''
+        if not fast:
+            return head.next
+        
+        while fast.next:
+                slow = slow.next
+                fast = fast.next
+        
+        slow.next = slow.next.next
+        
+        return head
+        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = head
+        fast = head
 
         if not head.next:
             return head.next
