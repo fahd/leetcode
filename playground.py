94d698b050ea6888e3897f8ee4dd3a4e0f089bb9
Live <live@Fahds-MacBook-Pro.local>
2024-12-07 18:39:19 -0500

    sql review

--- playground.py
+++ playground.py
@@ -1,36 +1,12 @@
 '''
-    1 - 2 - 3 - 4
+1 -> 2 -> 3 -> 4 -> 5
+          s
+                    f
 
-save = head.next.next
--> 3 - 4
-head.next.next = head
--> 2 -> 1
-        h
-prev = head
--> 2-> 1*
-
-node = save
-
-----------------------------
-
-node => 3 - 4
-if prev:
-    prev.next = node.next
-    -> 2 -> 1 -> 4
-
-if node.next:
-    save = node.next.next   
-    node.next.next = node
-else:
-    save = node.next
-
-node.next.next = node
-    -> 2 -> 1 -> 4 -> 3
-
-prev = node
-    -> 2 -> 1 -> 4 -> 3 
+''''
 
 
+def reverse_linked_list(list):
 
-'''
+
 
