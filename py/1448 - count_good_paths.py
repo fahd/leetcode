c5dc05efd214bf319d15d2f6ef35634456a94a94
Fahd <asadmansa@icloud.com>
2024-12-21 13:09:53 -0500

    100

--- py/1448 - count_good_paths.py
+++ py/1448 - count_good_paths.py
@@ -1,3 +1,20 @@
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+            new_max = max(max_so_far, node.val)
+            left = dfs(node.left, new_max)
+            right = dfs(node.right, new_max)
+            ans = left + right 
+            if node.val >= max_so_far:
+                ans += 1
+
+            return ans
+
+        
+        return dfs(root, float("-inf"))
+        
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
