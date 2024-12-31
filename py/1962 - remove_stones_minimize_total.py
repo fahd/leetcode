10ea083ed0e0e34b612531470217770a7121dd62
Fahd <asadmansa@icloud.com>
2024-12-30 23:20:53 -0500

    1962

---
+++ py/1962 - remove_stones_minimize_total.py
@@ -0,0 +1,23 @@
+import math
+from heapq import *
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        c = 0
+
+        piles = [-pile for pile in piles]
+        heapify(piles)
+
+        while c < k:
+            n = abs(-heappop(piles))
+            subtractor = math.floor(n // 2)
+            diff = abs(n - subtractor)
+            heappush(piles,-diff)
+            c+= 1
+        
+        s = sum([abs(pile) for pile in piles])
+        return s
+
+
+
+
+
