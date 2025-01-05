a9fe489162bca3493ef08c5800cfd3211a842873
Fahd <asadmansa@icloud.com>
2025-01-04 23:58:00 -0500

    ts

--- py/2294 - partition_array.py
+++ py/2294 - partition_array.py
@@ -1,3 +1,26 @@
+'''
+[3,6,1,2,5]
+->
+[1,2,3,5,6]
+ ^
+
+
+'''
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        n = 0
+        min_v = nums[0]
+        count = 1
+        
+        for i in range(1,len(nums)):
+            if nums[i] > min_v + k:
+                min_v = nums[i] 
+                count += 1
+
+        return count
+        
+# we want elements from [x, x+k]
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         mn = 0
